1e169ef13a2287f531b5356bb1dc310e6be634d5
grayayer <grayayer@gmail.com>
2015-07-07 15:04:15 -0700

    adding svg upload capability

--- functions.php
+++ functions.php
@@ -17,4 +17,33 @@ function child_hover_undo_hooks( $length ) {
 add_action( 'after_setup_theme', 'child_hover_undo_hooks' );
 
 
-add_action('wp_print_scripts', 'my_enqueue_scripts'); ?>
+add_action('wp_print_scripts', 'my_enqueue_scripts'); 
+
+
+##################################################################
+
+// enable images in media uploader
+
+##################################################################
+function cc_mime_types( $mimes ){
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter( 'upload_mimes', 'cc_mime_types' );
+
+##################################################################
+
+// display images on media uploader and feature images
+
+##################################################################
+
+function fix_svg_thumb_display() {
+  echo '<style> td.media-icon img[src$=".svg"], img[src$=".svg"].attachment-post-thumbnail { width: 100% !important; height: auto !important; } </style>';
+}
+add_action('admin_head', 'fix_svg_thumb_display');
+
+
+#########
+
+?>
+
